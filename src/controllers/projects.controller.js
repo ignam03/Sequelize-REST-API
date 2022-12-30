7892c2e4292162220def215673d35d9d35c0e5b6
Ignacio M <ignafree97@gmail.com>
2022-12-30 17:06:32 -0300

    [ADD]CRUD projects finish

--- src/controllers/projects.controller.js
+++ src/controllers/projects.controller.js
@@ -10,6 +10,23 @@ export const getProjects = async (req, res) => {
     }
 }
 
+export const getProject = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const project = await Project.findByPk(id);
+        if (!project) return res.status(404).json({ message:"project not found"})
+        res.json(project);
+        //otra forma de buscar un solo elemento
+        const project2 = await Project.findOne({
+            where: {
+                id
+            }
+        })
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
+
 export const createProject = async (req, res) => {
     const { name, priority, description } = req.body;
     try {
@@ -21,6 +38,37 @@ export const createProject = async (req, res) => {
         })
         res.json(newProject);
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        return res.status(500).json({ message: error.message });
+    }
+}
+
+export const updateProject = async (req, res) => {
+    try {
+        const { id } = req.params
+        const { name, priority, description } = req.body;
+        const project = await Project.findByPk(id)
+        project.name = name;
+        project.priority = priority;
+        project.description = description;
+        console.log(project);
+        await project.save();
+        res.json(project);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
+
+export const deleteProject = async (req, res) => {
+    try {
+        const { id } = req.params
+
+        await Project.destroy({
+            where: {
+                id
+            }
+        })
+        res.status(204)
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
     }
 }
